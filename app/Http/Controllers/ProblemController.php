623c4e0fda8f99aa4fe53627e39b9718e13a9a6e
HemaAnany <hema.3nany@gmail.com>
2019-02-18 23:49:27 +0200

    solve problem

---
+++ app/Http/Controllers/ProblemController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProblemController extends Controller
+{
+    public function numOfSteps($input)
+    {
+    	if ($input > 0) {
+	    	$output = ceil($input/5);
+	    	return $output;
+    	}else{
+    		return 'input must be greater than 0';
+    	}
+    }
+}
